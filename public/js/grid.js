e1031f7b4be077fb000576ab422246dd15e32864
EcceAgnusDei <mondo.antoine@yahoo.fr>
2019-06-27 11:58:30 +0200

    Création de la méthode fillRect de la classe grid

--- public/js/grid.js
+++ public/js/grid.js
@@ -4,8 +4,12 @@ class Grid
 	{
 		this.canvas = document.getElementById(canvasID);
 		this.ctx = this.canvas.getContext("2d");
-		this.line(30);
+		this.squareCoordX = [];
+		this.squareCoordY = [];
+		this.squareSize;
+		this.grid(30);
 		console.log("instanciation");
+		console.log(this.squareCoordX);
 
 		this.canvas.addEventListener("mousemove", function(evt) {
 			var mousePos = this.getMousePos(this.canvas, evt);
@@ -22,14 +26,19 @@ class Grid
 		};
 	}
 
-	line (squareSize)
+	grid (squareSize)
 	{
+		this.squareSize = squareSize;
 		console.log("dessin des lignes");
 		let x = 0;
 		let y = 0;
 
+		this.squareCoordX.push(0);
+		this.squareCoordY.push(0);
+
 		for (x = squareSize ; x < this.canvas.width ; x += squareSize)
 		{
+			this.squareCoordX.push(x);
 			this.ctx.beginPath();
 			this.ctx.moveTo(x, 0);
         	this.ctx.lineTo(x, this.canvas.height);
@@ -39,6 +48,7 @@ class Grid
 
 		for (y = squareSize ; y < this.canvas.height ; y += squareSize)
 		{
+			this.squareCoordY.push(y);
 			this.ctx.beginPath();
 			this.ctx.moveTo(0, y);
         	this.ctx.lineTo(this.canvas.width, y);
@@ -46,6 +56,38 @@ class Grid
         	this.ctx.stroke();
 		}
 	}
+
+	fillRect(x, y)
+	{
+		var beginX = 0;	
+		var beginY = 0;
+
+		for (let i=0 ; i < this.squareCoordX.length ; i++)
+		{
+			if (this.squareCoordX[i] + this.squareSize <= x)
+			{
+				beginX = this.squareCoordX[i];
+				console.log('coucou i');
+			}
+		
+			for (let j=0 ; j < this.squareCoordY.length ; j++)
+			{
+				if (this.squareCoordY[j] + this.squareSize <= y)
+				{
+					beginY = this.squareCoordX[j];
+					console.log('coucou j');
+				}
+			}
+		}
+		
+		this.ctx.beginPath();
+		this.ctx.rect(beginX, beginY,this.squareSize, this.squareSize);
+		this.ctx.fillStyle="black";
+  		this.ctx.fill();
+  		console.log(beginX);
+  		console.log(beginY);
+	}
 }
 
 let grid = new Grid("canvas");
+grid.fillRect(30, 60);
