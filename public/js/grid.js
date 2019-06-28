18dafec8f1a52fb59462159a199fe46d6e405064
EcceAgnusDei <mondo.antoine@yahoo.fr>
2019-06-28 12:30:09 +0200

    Rassemblement des coordlog

--- public/js/grid.js
+++ public/js/grid.js
@@ -4,17 +4,26 @@ class Grid
 	{
 		this.canvas = document.getElementById(canvasID);
 		this.ctx = this.canvas.getContext("2d");
-		this.squareCoordX = [];
-		this.squareCoordY = [];
+		this.squareCoord = [];
+		this.isBlack = [];
 		this.squareSize;
+
+		this.ctx.beginPath();
+		this.ctx.rect(0, 0,this.canvas.width, this.canvas.height);
+		this.ctx.fillStyle = 'white';
+  		this.ctx.fill();
+
 		this.grid(30);
-		console.log("instanciation");
-		console.log(this.squareCoordX);
 
 		this.canvas.addEventListener("mousemove", function(evt) {
-			var mousePos = this.getMousePos(this.canvas, evt);
-			console.log(mousePos.x);
-			console.log(mousePos.y);
+			let mousePos = this.getMousePos(this.canvas, evt);
+		}.bind(this));
+
+		this.canvas.addEventListener("click", function(evt){
+			let mousePos = this.getMousePos(this.canvas, evt);
+			// console.log(mousePos.x);
+			// console.log(mousePos.y);
+			this.fillRect(mousePos.x, mousePos.y, 'black');
 		}.bind(this));
 	}
 
@@ -30,26 +39,26 @@ class Grid
 	{
 		this.squareSize = squareSize;
 		console.log("dessin des lignes");
-		let x = 0;
-		let y = 0;
 
-		this.squareCoordX.push(0);
-		this.squareCoordY.push(0);
-
-		for (x = squareSize ; x < this.canvas.width ; x += squareSize)
+		for (let x = 0 ; x < this.canvas.width ; x += squareSize)
 		{
-			this.squareCoordX.push(x);
 			this.ctx.beginPath();
+			this.ctx.strokeStyle = "black";
 			this.ctx.moveTo(x, 0);
         	this.ctx.lineTo(x, this.canvas.height);
         	this.ctx.closePath();
         	this.ctx.stroke();
+        	for (let y = 0 ; y < this.canvas.height ; y += squareSize)
+        	{
+        		this.squareCoord.push([x, y]);
+        		this.isBlack.push(0);
+        	}
 		}
 
-		for (y = squareSize ; y < this.canvas.height ; y += squareSize)
+		for (let y = 0 ; y < this.canvas.height ; y += squareSize)
 		{
-			this.squareCoordY.push(y);
 			this.ctx.beginPath();
+			this.ctx.strokeStyle = "black";
 			this.ctx.moveTo(0, y);
         	this.ctx.lineTo(this.canvas.width, y);
         	this.ctx.closePath();
@@ -57,37 +66,87 @@ class Grid
 		}
 	}
 
-	fillRect(x, y)
+	fillRect(x, y, color)
 	{
-		var beginX = 0;	
-		var beginY = 0;
-
-		for (let i=0 ; i < this.squareCoordX.length ; i++)
+		console.log("Appel");
+		console.log(this.squareCoord[10]);
+		console.log(this.squareCoord[11]);
+		for (let i = 0 ; i < this.squareCoord.length ; i++)
 		{
-			if (this.squareCoordX[i] + this.squareSize <= x)
+			if (x > this.squareCoord[i][0] && x < this.squareCoord[i][0] + this.squareSize && 
+				y > this.squareCoord[i][1] && y < this.squareCoord[i][1] + this.squareSize)
 			{
-				beginX = this.squareCoordX[i];
-				console.log('coucou i');
+				if(color == 'black') {this.isBlack[i] = 1;}
+				let beginX = this.squareCoord[i][0];
+				let beginY = this.squareCoord[i][1];
+				this.ctx.beginPath();
+				this.ctx.rect(beginX+1, beginY+1,this.squareSize-2, this.squareSize-2);
+				this.ctx.fillStyle = color;
+				this.ctx.fill();
 			}
-		
-			for (let j=0 ; j < this.squareCoordY.length ; j++)
+		}
+	}
+
+	isBlack (x, y)
+	{
+		let pixelData = this.ctx.getImageData(x, y, 1, 1).data;
+		if (pixelData[0] === 0 && pixelData[3] === 255)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	countNeighbors(abs, ord)
+	{
+		let count = 0;
+		for (let x = abs - this.squareSize ; x <= abs + this.squareSize ; x += this.squareSize)
+		{
+			for (let y = ord - this.squareSize ; y <= ord + this.squareSize ; y += this.squareSize)
 			{
-				if (this.squareCoordY[j] + this.squareSize <= y)
+				if (this.isBlack(x, y) && (x != abs || y!= ord))
 				{
-					beginY = this.squareCoordX[j];
-					console.log('coucou j');
+					count++;
 				}
 			}
 		}
-		
-		this.ctx.beginPath();
-		this.ctx.rect(beginX, beginY,this.squareSize, this.squareSize);
-		this.ctx.fillStyle="black";
-  		this.ctx.fill();
-  		console.log(beginX);
-  		console.log(beginY);
+		return count;
+	}
+
+	next ()
+	{
+		console.log("t++");
+		for (let i=0 ; i < this.squareCoordX.length ; i++)
+		{
+			for (let j=0 ; j < this.squareCoordY.length ; j++)
+			{
+				let x = this.squareCoordX[i] - this.squareSize / 2;
+				let y = this.squareCoordY[j] - this.squareSize / 2;
+				if (this.countNeighbors(x, y) === 3)
+				{
+					this.fillRect(x, y, 'black');
+				}
+				else if (this.isBlack(x, y))
+				{
+					console.log("black");
+					if (this.countNeighbors(x, y) > 3 || this.countNeighbors(x, y) < 2)
+					{
+						this.fillRect(x, y, 'white');
+					}
+				}
+				
+			}
+		}
 	}
 }
 
 let grid = new Grid("canvas");
-grid.fillRect(30, 60);
+// grid.fillRect(30, 60, 'black');
+// grid.fillRect(30, 90, 'black');
+// grid.fillRect(30, 120, 'black');
+// grid.fillRect(30, 150, 'black');
+// grid.fillRect(75, 135, 'black');
+console.log(grid.squareCoord);
