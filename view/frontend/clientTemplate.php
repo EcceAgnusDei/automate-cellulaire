03d46d9d628dad71c1879b097fc2d2a8045fbf18
EcceAgnusDei <mondo.antoine@yahoo.fr>
2019-07-11 15:02:23 +0200

    Création d'un lien déconnexion pour l'administrateur coté client

--- view/frontend/clientTemplate.php
+++ view/frontend/clientTemplate.php
@@ -92,7 +92,22 @@
     	</header>
         <?= $content ?>
         <footer class="client-footer">
-            <p class="footer-content grid">&copy; 2019 Antoine Mondoloni . <a href="view/frontend/rgpd.html">RGPD</a> . <a href="view/frontend/legalNotice.html">Mentions légales</a> . <?php if(isset($_SESSION['userid'])) {?><button class="link-btn" onclick='window.location.href="index.php?action=userdelete&id=<?= $_SESSION['userid'] ?>"'>Supprimer votre compte</button> . <?php }  ?><a href="index.php?action=adminlogin">Admin</a></p>
+            <p class="footer-content grid">&copy; 2019 Antoine Mondoloni . <a href="view/frontend/rgpd.html">RGPD</a> . <a href="view/frontend/legalNotice.html">Mentions légales</a> . <?php 
+            if(isset($_SESSION['userid']) && !isset($_SESSION['admin'])) 
+            {
+            ?>
+                <button class="link-btn" onclick='window.location.href="index.php?action=userdelete&id=<?= $_SESSION['userid'] ?>"'>Supprimer votre compte</button> . 
+            <?php 
+            }  
+            else if(isset($_SESSION['admin']))
+            {
+            ?>
+                <a href="index.php?adminaction=adminlogout">Deconnexion</a> . 
+            <?php 
+            }
+            ?>
+            <a href="index.php?action=adminlogin">Admin</a></p>
         </footer>
     </body>
 </html>
+<a href="index.php?adminaction=adminlogout">Deconnexion</a>
