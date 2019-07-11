e3b4d8089dbf219da5f21e6927495845f0f3b7b7
EcceAgnusDei <mondo.antoine@yahoo.fr>
2019-07-11 15:43:19 +0200

    Responsivité du header pour portable

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -3,7 +3,7 @@ window.addEventListener("load", buttons);
 function buttons()
 {
 	$('.login-btn').click(function(){
-		$('.user-login-form').css('display','block'); 
+		$('.user-login-form').css('display','flex'); 
 		$('#forgotten-form').css('display','none');
 		$('.signin-link').css('display','block');
 		if ($('.header-forms').css('display') === 'none')
